3e75a4ac22f62c0461425f0ccabc8a30803138d0
Mitama <yussa.de.dannann@gmail.com>
2022-02-14 18:33:35 +0900

    refactor(parse): remove more redundant lifetimes

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -19,10 +19,10 @@ fn any_char(input: &str) -> Option<(char, &str)> {
 
 // 条件を渡すとパーサーを作ってくれる
 #[allow(dead_code)]
-fn sat<'a>(
+fn sat(
     pred: impl Fn(char) -> bool,
-) -> impl FnOnce(&'a str) -> Option<(char, &'a str)> {
-    move |input| -> Option<(char, &'a str)> {
+) -> impl FnOnce(&str) -> Option<(char, &str)> {
+    move |input| -> Option<(char, &str)> {
         any_char(input).and_then(|(parsed, rest)| pred(parsed).then(|| (parsed, rest)))
     }
 }
